e133974338e3e22c7b10295b5c25c4be19c7d4c8
CODENAME883 <92009379+CODENAME883@users.noreply.github.com>
2021-10-13 17:49:10 -0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -24,15 +24,7 @@ export default function Home() {
 
 
       <div id="root">
-          <div className="flex items-center justify-center w-full border-b-2 border-r-8 border-opacity-0 border-l-8	pb-6 border-opacity-0">
-            <a href="/" className=""><img src="images/logo_transparent.png" width="108" alt="" className="logo_transparent-image" /></a>
-            <nav className="flex flex-wrap flex-row justify-around PermanentMarker-Regular">
-              <a href="/mint" className="text-4xl text-black hover:underline m-6">Mint</a>
-              <a href="#about" className="text-4xl text-black hover:underline m-6">About</a>
-              <a href="#rarity" className="text-4xl text-black hover:underline m-6">Rarity</a>
-              <a href="#roadmap" className="text-4xl text-black hover:underline m-6">Roadmap</a>
-            </nav>
-          </div>
+
 
           <div className="flex items-center justify-center w-full border-b-2 border-r-8 border-opacity-0 border-l-8	pb-6 border-opacity-0">
             <nav className="flex flex-wrap flex-row justify-around PermanentMarker-Regular">
@@ -42,8 +34,7 @@ export default function Home() {
           </div>
 
           <div className="flex flex-col items-center border-b-20 ">
-            <img src="images/slowmate_creation_timelapse.gif"/>
-            <a href="/mint" className="mt-4 PermanentMarker-Regular text-4xl border-6 bg-white rounded-xl ring-4 text-black hover:text-black p-2 ">MINT</a>
+            <img src="images/collage.png"/>
           </div>
 
           <div className="flex flex-col items-center">
@@ -51,8 +42,8 @@ export default function Home() {
           <div className="mt-6 border-b-2 py-6 border-l-8	pb-6 border-opacity-0">
               <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
                 <div className="lg:w-full">
-                  <h1 className="text-4xl PermanentMarker-Regular text-black font-bold"></h1>
-                  <p className="text-2xl PermanentMarker-Regular text-black ">
+                  <h1 className="text-2xl PermanentMarker-Regular text-black font-bold"></h1>
+                  <p className="text-lg PermanentMarker-Regular text-black ">
                  </p>
                     <img className="lg:w-full" src="images/divide.png" />
                 </div>
@@ -62,11 +53,11 @@ export default function Home() {
             <div className="mt-6 border-b-2 py-6 border-l-8	pb-6 border-opacity-0">
               <div id="about" className="flex flex-wrap lg:flex-nowrap justify-around items-center">
                 <div className="lg:w-full">
-                  <h1 className="text-4xl PermanentMarker-Regular text-black font-bold">What are Slowmates? </h1>
-                  <p className="text-2xl PermanentMarker-Regular text-black ">
+                  <h1 className="text-2xl PermanentMarker-Regular text-black font-bold">What are Slowmates? </h1>
+                  <p className="text-lg PermanentMarker-Regular text-black ">
                   "Slowmates are 7,777 unique sloth NFTs coming soon to the Ethereum blockchain. Each Slowmate is a unique combination of 5 random sloth traits of different rarities: fur, eyes, mouth, head, and body.  The random combinations are endless to ensure there is enough variety for everyone.  Mint your NFT so that you can find your Slowmate."
                   </p>
-                  <img className="lg:w-full" src="images/collage.png" />
+                  <img className="lg:w-full" src="images/slowmate_creation_timelapse.gif" />
                 </div>
               </div>
             </div>
@@ -74,8 +65,8 @@ export default function Home() {
             <div className="mt-6 border-b-2 py-6 border-l-8	pb-6 border-opacity-0">
               <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
                 <div className="lg:w-full">
-                  <h1 className="text-4xl PermanentMarker-Regular text-black font-bold"></h1>
-                  <p className="text-2xl PermanentMarker-Regular text-black ">
+                  <h1 className="text-2xl PermanentMarker-Regular text-black font-bold"></h1>
+                  <p className="text-lg PermanentMarker-Regular text-black ">
                  </p>
                     <img className="lg:w-full" src="images/divide.png" />
                 </div>
@@ -85,8 +76,8 @@ export default function Home() {
             <div className="mt-6 border-b-2 py-6 border-l-8	pb-6 border-opacity-0">
               <div id="rarity" className="flex flex-wrap lg:flex-nowrap justify-around items-center">
                 <div className="lg:w-full">
-                  <h1 className="text-4xl PermanentMarker-Regular text-black font-bold">How rare is my Slowmate? </h1>
-                  <p className="text-2xl PermanentMarker-Regular text-black ">
+                  <h1 className="text-2xl PermanentMarker-Regular text-black font-bold">How rare is my Slowmate? </h1>
+                  <p className="text-lg PermanentMarker-Regular text-black ">
                     "The rarity of your Slowmate is determined by the rarity of the traits.  Each trait has a unique rarity which contributes to your specific Slowmate's rarity.  Each individual trait has a rarity score based on how many times that trait shows up in the Slowmate population.  The overall rarity score is simply the summation of the each of the rarity score for each individual trait."
                   </p>
                   <img className="lg:w-full" src="images/rarity_board.png" />
@@ -97,8 +88,8 @@ export default function Home() {
             <div className="mt-6 border-b-2 py-6 border-l-8	pb-6 border-opacity-0">
               <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
                 <div className="lg:w-full">
-                    <h1 className="text-4xl PermanentMarker-Regular text-black font-bold">So, is there a quick way to determine rarity? </h1>                   
-                    <p className="text-2xl PermanentMarker-Regular text-black ">
+                    <h1 className="text-2xl PermanentMarker-Regular text-black font-bold">So, is there a quick way to determine rarity? </h1>                   
+                    <p className="text-lg PermanentMarker-Regular text-black ">
                     "The background color of your Slowmate is an easy way to determine relative rarity in the Slowmate population. Each color represents a group, a tier, and a numeric point value.                   </p>
                     <img className="lg:w-full" src="images/rarity.png" />
                 </div>
@@ -108,8 +99,8 @@ export default function Home() {
             <div className="mt-6 border-b-2 py-6 border-l-8	pb-6 border-opacity-0">
               <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
                 <div className="lg:w-full">
-                  <h1 className="text-4xl PermanentMarker-Regular text-black font-bold"></h1>
-                  <p className="text-2xl PermanentMarker-Regular text-black ">
+                  <h1 className="text-2xl PermanentMarker-Regular text-black font-bold"></h1>
+                  <p className="text-lg PermanentMarker-Regular text-black ">
                  </p>
                     <img className="lg:w-full" src="images/divide.png" />
                 </div>
@@ -119,8 +110,8 @@ export default function Home() {
             <div className="mt-6 border-b-2 py-6 border-l-8	pb-6 border-opacity-0">
               <div id="roadmap" className="flex flex-wrap lg:flex-nowrap justify-around items-center">
                 <div className="lg:w-full">
-                  <h1 className="text-4xl PermanentMarker-Regular text-black font-bold">Do you have a roadmap? </h1>
-                  <p className="text-2xl PermanentMarker-Regular text-black ">
+                  <h1 className="text-2xl PermanentMarker-Regular text-black font-bold">Do you have a roadmap? </h1>
+                  <p className="text-lg PermanentMarker-Regular text-black ">
                   "The waterfall represents the start of the Slowmate journey.  The waterfall displays both nature's beauty and power.  Climbing up the waterfall and crossing the bridge will unlock additional roadmap objectives while making it to the top will unlock the project's first real-world charitable donation."                  </p>
                   <img className="lg:w-full" src="images/W1S4.png" />
                   <img className="lg:w-full" src="images/W1D.png" />
