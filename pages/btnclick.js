47740d57d54bdbd6f370c7326f33c03704f3c1c7
CODENAME883 <92009379+CODENAME883@users.noreply.github.com>
2021-10-24 00:45:41 -0700

    Add files via upload

---
+++ pages/btnclick.js
@@ -0,0 +1,22 @@
+
+/*
+if (typeof window === 'object') {
+    // Check if document is finally loaded
+       document.addEventListener("DOMContentLoaded", function () {
+           //alert('Finished loading')
+            const burger = document.body.querySelector('#burger');
+            const menu = document.body.querySelector('#menu');
+
+            burger.addEventListener('click', () => {
+            if(menu.classList.contains('hidden')) {
+                menu.classList.remove('hidden');
+            } else {
+                menu.classList.add('hidden')
+            }
+
+            })
+         });
+      }
+      
+*/
+        
